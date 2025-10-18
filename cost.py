fcba12dfc58079d71eff235ac64285c3943098e4
bdg221 <brian.goldsmith@gmail.com>
2025-10-17 20:09:06 -0700

    Add another cost method using simulated results

--- cost.py
+++ cost.py
@@ -2,9 +2,13 @@
 
 import numpy as np
 from qiskit import QuantumCircuit, transpile
+from qiskit.result import Result
 from qiskit_aer import AerSimulator
+from itertools import product
 
 from generate_circuits import generate_toffoli
+
+simulator = AerSimulator()
 
 
 def frobenius_norm(circuit: QuantumCircuit) -> float:
@@ -47,3 +51,45 @@ def trace_norm(circuit: QuantumCircuit) -> float:
     return 0.5 * np.linalg.norm(
         toffoli_unitary @ unitary.conj().T - np.eye(toffoli_unitary.shape[0]), "fro"
     )
+
+
+def count_comparison(circuit: QuantumCircuit) -> float:
+    """This method simulates all possible inputs and compares the count results
+    against the ideal Toffoli results. The number"""
+    toffoli_counts = run_all_inputs(generate_toffoli())
+    all_inputs_counts = run_all_inputs(circuit)
+    total_cost = float(0)
+    for bits in toffoli_counts.keys():
+        toffoli_probabilities = toffoli_counts[bits]
+        all_inputs_probabilties = all_inputs_counts[bits]
+        combined_probabilites = set(toffoli_probabilities.keys()) | set(
+            all_inputs_probabilties.keys()
+        )
+        cost = sum(
+            abs(toffoli_probabilities.get(o, 0) - all_inputs_probabilties.get(o, 0))
+            for o in combined_probabilites
+        )
+        total_cost += cost
+    return total_cost / len(toffoli_counts)
+
+
+def run_all_inputs(circuit: QuantumCircuit) -> dict:
+    """This function runs the circuit with all possible inputs for 3 qubits. The results
+    are returned as dictionary"""
+    results = {}
+    for input_vals in product([0, 1], repeat=3):
+        temp_circuit = QuantumCircuit(3, 3)
+        for i, bit in enumerate(input_vals):
+            if bit:
+                temp_circuit.x(i)
+        temp_circuit.append(circuit, [0, 1, 2])
+        temp_circuit.measure_all(add_bits=False)
+        counts = (
+            simulator.run(transpile(temp_circuit, simulator, optimization_level=0))
+            .result()
+            .get_counts()
+        )
+        total = sum(counts.values())
+        probabilities = {bits: count / total for bits, count in counts.items()}
+        results["".join(map(str, input_vals))] = probabilities
+    return results
